2a07407e1fc57cd57a8aaad8f2166b893a8bdd11
820627765 <820627765@qq.com>
2021-05-16 15:53:34 +0800

    update

--- microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/DeptProvider8001_App.java
@@ -2,9 +2,11 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient   //启用我是Eureka 的客户端，当应用启动时，会根据配置发送心跳给 Eureka 服务端
+@EnableDiscoveryClient //用于服务发现
 @SpringBootApplication
 public class DeptProvider8001_App {
 	public static void main(String[] args) {
--- microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/example/springcloud/controller/DeptController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.example.springcloud.entity.Dept;
 import com.example.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeptController{
 	@Autowired
 	private DeptService service;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept)
@@ -32,5 +36,21 @@ public class DeptController{
 	public List<Dept> list()
 	{
 		return service.list();
+	}
+
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery(){
+		//通过 discoveryClient 去 eureka 查看有哪些微服务，返回微服务列表
+		List<String> list = discoveryClient.getServices();
+		System.out.println("**********" + list);
+
+		// 通过 discoveryClient 的getInstances 方法 通过服务名称查询 是这个服务名称的 微服务实例列表（一个服务的集群）。
+		List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			//通过微服务实例访问微服务的Id，服务的主机，服务的端口号，服务的uri 等信息
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.discoveryClient;
 	}
 }
